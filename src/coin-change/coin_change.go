da18dcf11f79f2d8cb2c4ebdb3bfc6fcd2988d35
Cristian Lopez <cristian@getrentcheck.com>
2025-05-06 12:34:40 -0300

    coin change, adding cases, starting new solution since the previous started to become too complex

--- src/coin-change/coin_change.go
+++ src/coin-change/coin_change.go
@@ -1,34 +1,5 @@
 package coin_change
 
 func CoinChange(coinDenominations []int32, amount int32) int32 {
-	var i, newAmount, coinCount int32
 
-	coinCount = 0
-	newAmount = amount
-
-	denominationsLength := int32(len(coinDenominations))
-
-	/*
-		We loop from the biggest denomination and check how many of that we need.
-		With that value, we subtract the amount and proceed to the following
-		denomination.
-	*/
-	for i = denominationsLength - 1; i > -1; i-- {
-		denominationAmount := newAmount / coinDenominations[i]
-
-		if denominationAmount > 0 {
-			coinCount += denominationAmount
-			newAmount -= denominationAmount * coinDenominations[i]
-		}
-	}
-
-	/*
-		If new amount could not be set to 0, means there is no way to give the
-		change
-	*/
-	if newAmount == 0 {
-		return coinCount
-	}
-
-	return -1
 }
--- src/coin-change/coin_change_test.go
+++ src/coin-change/coin_change_test.go
@@ -5,17 +5,46 @@ import (
 	"testing"
 )
 
-func TestCoinChange(t *testing.T) {
+func TestPossibleCoinChange(t *testing.T) {
 	datatable := []struct {
 		coinDenominations []int32
 		amount            int32
 		expected          int32
 	}{
 		{[]int32{1, 2, 5}, 11, 3},
-		{[]int32{2}, 3, -1},
 		{[]int32{1}, 3, 3},
-		{[]int32{2, 5}, 8, -1},
 		{[]int32{1}, 0, 0},
+		{[]int32{1, 3, 4}, 4, 1},
+		{[]int32{1, 3, 4}, 6, 2},
+		{[]int32{1, 2, 5}, 100, 20},
+		{[]int32{1}, 1000, 1000},
+		{[]int32{2, 5}, 8, 4},
+		{[]int32{2, 5}, 11, 4}, // 5:1, 2:3
+	}
+
+	for _, dataRow := range datatable {
+		t.Run(fmt.Sprintf(
+			"for denominations = %v and amount %d, we expected %d coints",
+			dataRow.coinDenominations, dataRow.amount, dataRow.expected,
+		), func(t *testing.T) {
+			result := CoinChange(dataRow.coinDenominations, dataRow.amount)
+
+			if result != dataRow.expected {
+				t.Errorf("expected %d, got %d", dataRow.expected, result)
+			}
+		})
+
+	}
+}
+
+func TestImpossibleCoinChange(t *testing.T) {
+	datatable := []struct {
+		coinDenominations []int32
+		amount            int32
+		expected          int32
+	}{
+		{[]int32{2}, 3, -1},
+		{[]int32{5, 7}, 1, -1},
 	}
 
 	for _, dataRow := range datatable {
