1ddbc29f6b844514e43269a04f770751b3d26703
Cristian Lopez <cristian@getrentcheck.com>
2025-05-06 21:09:36 -0300

    adding comment to explain the algorithm

--- src/coin-change/coin_change.go
+++ src/coin-change/coin_change.go
@@ -21,22 +21,35 @@ func CoinChange(coinDenominations []int, amount int) int {
 		changeByAmount[currentAmount] = -1
 	}
 
+	// From here we use dynamic programming method, which solves the main problem
+	// by dividing it in smaller ones. From a smaller ones we reach the main one.
+
+	// Using the coin denominations list, we go from amount 1 to the provided
+	// amount. The solution for amount 1 will be used to get the solution for
+	// amount 2, and so on.
 	for currentAmount := 1; currentAmount < changeByAmountLen; currentAmount++ {
 
 		for i := 0; i < coinDenominationsLen; i++ {
 			diff := currentAmount - coinDenominations[i]
 
+			// If the current amount is smaller than the coin denomination,
+			// end the loop
 			if diff < 0 {
 				break
 			}
 
+			// If there is no solution on the previous step, move on
 			if changeByAmount[diff] == -1 {
 				continue
 			}
 
 			if changeByAmount[currentAmount] == -1 {
+				// In here we need add one coin (from the current denomination
+				// from the loop) to the first previous solution
 				changeByAmount[currentAmount] = changeByAmount[diff] + 1
 			} else {
+				// This case represents we have one solution but we are looking
+				// for the smaller amount of coins
 				if changeByAmount[currentAmount] > (changeByAmount[diff] + 1) {
 					changeByAmount[currentAmount] = changeByAmount[diff] + 1
 				}
@@ -45,5 +58,7 @@ func CoinChange(coinDenominations []int, amount int) int {
 
 	}
 
+	// Reached the end! we just return the last element of the list, which was
+	// built using all previous solutions
 	return changeByAmount[amount]
 }
